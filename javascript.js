45f7e64c7673949d6c5b498ed1f90791ef903927
Kelly <kbc23@njit.edu>
2024-11-26 09:49:30 -0500

    create javascript.js

---
+++ javascript.js
@@ -0,0 +1,31 @@
+//question 1
+function greetUser(Username){
+    document.getElementById('output').innerText = "Hello," +Username;
+}
+
+greetUser('Harry')
+
+//question 2
+function buttonchanges(){
+    document.getElementById('message').innerText = "Hello, World!"
+}
+
+//question 3 
+function addedresult(){
+    let a = 10;
+    let b = 100;
+    console.log(a + b);
+    document.getElementById('heading-two').innerText = (a+b);
+    document.getElementById('para1').innerHTML = '<h1>This is final sum</h1>';
+}
+
+//question 4
+function inputchange(){
+    document.getElementById('insertinput').innerText;
+    document.getElementById('title').innerHTML = '<h1>This is final sum</h1>';
+}
+
+//question 5 
+
+
+//question 6
