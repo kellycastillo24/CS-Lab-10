1e683e3531706b5dfdc5796ac31291f425d40953
Kelly <kbc23@njit.edu>
2024-11-26 21:38:22 -0500

    update javascript.js

--- javascript.js
+++ javascript.js
@@ -11,21 +11,44 @@ function buttonchanges(){
 }
 
 //question 3 
-function addedresult(){
-    let a = 10;
-    let b = 100;
-    console.log(a + b);
-    document.getElementById('heading-two').innerText = (a+b);
-    document.getElementById('para1').innerHTML = '<h1>This is final sum</h1>';
+function addNumbers(){
+    const num1 = parseFloat (document.getElementById('num1').value);
+    const num2 = parseFloat (document.getElementById('num2').value);
+
+    const sum = num1 + num2;
+
+    document.getElementById('result').textContent = `The result is: ${sum}`;
 }
+
 
 //question 4
-function inputchange(){
-    document.getElementById('insertinput').innerText;
-    document.getElementById('title').innerHTML = '<h1>This is final sum</h1>';
-}
+const inputField = document.getElementById('inputField');
+const updateButton = document.getElementById('updateButton');
+const title = document.getElementById('title');
+
+//event listener updates in real time
+updateButton.addEventListener('click', () => {
+   
+    title.textContent = inputField.value;
+});
 
 //question 5 
+const inputchange1 = document.getElementById('inputchange1');
+const inputchange2 = document.getElementById('inputchange2');
+const concatButton = document.getElementById('concatButton');
+const combinedText = document.getElementById('combinedText');
 
+concatButton.addEventListener('click', () => {
+    
+    const result = inputchange1.value + " " + inputchange2.value;
+    combinedText.textContent = result;
+});
 
 //question 6
+const changeColorButton = document.getElementById('changeColorButton');
+const colorBox = document.getElementById('colorBox');
+
+changeColorButton.addEventListener('click', () => {
+    
+    colorBox.style.backgroundColor = 'blue';
+});
